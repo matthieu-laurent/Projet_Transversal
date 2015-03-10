5cca2c938f7a58129c9b69757ee68f5b69848e71
stancpe <stan_renia@hotmail.com>
2015-03-10 21:04:46 +0100

    Update ultrasonic.c
    Ajout de constantes
    Amélioration de l'algorithme.

--- Module_Telemetre/ultrasonic.c
+++ Module_Telemetre/ultrasonic.c
@@ -1,20 +1,31 @@
-/******************************************************************
-*******************Projet Transversal******************************
-*****************Module : Capteur a ultrasons**********************
-***********************Version 1***********************************
-**************Date de derniere modification : 05.03.2015***********
-******************************************************************/*
+#include <LIB_BSE_Config_Global.h>
+#include <intrins.h>
+#include <stdio.h>
 
-#include <C8051F020.H>
-#include <intrins.h> //permet d'utiliser les fonctions _nop_() pour la temporisation
+// **** ultrason.h **************************
+#define ULTRASON_COEF 59 // distance = durée_mesurée*340.29 (m/s) / 2 <=> distance = 0,017015 * durée_mesurée = durée_mesurée/59
+#define ULTRASON_DMIN 2 // 2cm
+#define ULTRASON_DMAX 400	// 400 cm
+#define ULTRASON_TMIN  1*ULTRASON_COEF*ULTRASON_DMIN	// durée minimale mesurée pour laquelle la mesure est correcte (2 cm)
+#define ULTRASON_TMAX  1*ULTRASON_COEF*ULTRASON_DMAX	// durée maximale mesurée pour laquelle la mesure est correcte (400 cm)
 
 sfr16 DPTR=0x82;
-sbit trig=P3^5; //on declare une variable trig representant le trigger sur le 5eme bit de la broche P3^5
+sbit trig=P2^0; // P2.0 en d:0xa0
+sbit INT0=P2^1; // crossbar à faire avec wizard
+// **** fin ultrason.h **************************
 
+/*
+P2MDOUT   = 0x7B; // Configuration des bits du port 2
+P3MDOUT   = 0xFC; // Configuration des bits du port 3
+	
+Les 3 lignes suivantes permettent d'activer le crossbar et de router INT1 sur P1.2
+	  XBR0      = 0x07;
+    XBR1      = 0x10;
+		
+EX0 = 1;		
+*/
 void envoi_pulsation(void) //fonction permettant de générer un delai de 10us pour declencher le trigger
 {
-	TH0=0x00;
-	TL0=0x00;
 	trig=1;
 	_nop_(); //1
 	_nop_(); //2
@@ -29,37 +40,67 @@ void envoi_pulsation(void) //fonction permettant de générer un delai de 10us p
 	trig=0;
 }
 
-unsigned char calcul_distance(void)
+unsigned short calcul_distance(void)
 {
-	unsigned char distance;
+	unsigned short distance;
 	envoi_pulsation();
 	
 	while(!INT0); //permet de generer un delai de 40ms
-	while(INT0)
+	TH0=0x00; // mise à 0 Timer0, démarrage mesure durée impulsion Echo du capteur ultrason.
+	TL0=0x00; // TL0 en D:0x8a et TH0 en D:0x8c
+	while(INT0) // INT0 = P2 en 0xA0
 	{
 		DPH=TH0;
 		DPL=TL0;
 	}
-	TH0=0xFF;
-	TL0=0xFF;
-	if(DPTR<38000) //possible que le cpateur ne marche pas a ce niveau, dans ce cas prendre 35000
-		distance=DPTR/58,771; //voir calcul, distance en cm
+
+	if(DPTR>=ULTRASON_TMIN)  // Si durée mesurée correcte, obstacle > 2cm
+	{
+		if(DPTR<=ULTRASON_TMAX) // Si durée mesurée correcte, obstacle < 400cm
+		{
+			distance=DPTR/ULTRASON_COEF; // calcule la distance mesurée
+		}
+		else // sinon, obstacle > 400cm
+		{
+			distance = ULTRASON_DMAX; // distance = 400 cm : on peut avancer de 400 cm.
+		}
+	}
 	else
-		distance=0; //pas d'obstacle devant le capteur
+	{
+		distance = 0; // Obstacle juste devant le capteur (à moins de 2cm)
+	}
+		 
+	return distance;
+}
+
+void delay(unsigned short duree) // delai logiciel pour tester
+{
+	unsigned short i;
+	
+	for(i=0;i<duree;i++)
+	{
+		_nop_();
+	}
 }
 
 void main()
 {
-	TMOD=0x09; // timer0 en mode 2 (16bit Counter/timer)
-	TR0=1; // mise en marche du timer0
-	TH0=0x00;
-	TL0=0x00;
-	P3|=0x04; // configuration du pin P3.5 pour en faire une entrée
+	unsigned short distance_cible=0;
 	
-	unsigned int distance_cible=0;
-	while(1)
+	Init_Device();
+	
+	// Init Timer 0
+	TMOD|=0x09; // timer0 en mode 2 (16bit Counter/timer)
+	TMOD&=0xF9; // timer0 en mode 2 (16bit Counter/timer)
+	CKCON|=0x08; // system clock
+	IT0 = 1; // INT0 is edge triggered
+	TR0=1; // mise en marche du timer0
+	
+	
+	while(1) // boucle de test
 	{
 		distance_cible=calcul_distance();
 		printf("%d\n", distance_cible);
+		delay(20000);
 	}
 }
