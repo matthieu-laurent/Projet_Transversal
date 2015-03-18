2207d7c8c00d2dc0b96985c6080c5db5c0e44535
victorfaramond <victor.faramond@gmail.com>
2015-03-18 18:24:19 +0100

    Create LIB_BSE_UART.h

---
+++ Module_M1/LIB_BSE_UART.h
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------------
+// Projet BSE 2014 - Fichier configuration et gestion de l'UART0
+//------------------------------------------------------------------------------------
+//
+// AUTH: FARAMOND Victor & CHENE Charly
+// DATE: 06-10-2014
+// Target: C8051F02x
+
+void CFG_Clock_UART0();
+
+void CFG_UART0();
+
+char Putchar(char c, char csg_tempo);
+
+char Send_String(char *char_ptr);
+
+char Getchar();
+
+void Demo_UART();
