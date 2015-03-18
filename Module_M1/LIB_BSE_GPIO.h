f48bcaac779a655296d0e9232cbeebb86da8b8cd
victorfaramond <victor.faramond@gmail.com>
2015-03-18 18:25:20 +0100

    Create LIB_BSE_GPIO.h

---
+++ Module_M1/LIB_BSE_GPIO.h
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------------
+// Projet BSE 2014 - Fonctions de configuration et d'utilisation des GPIOs
+//------------------------------------------------------------------------------------
+//
+// AUTH: FARAMOND Victor & CHENE Charly
+// DATE: 06-10-2014
+// Target: C8051F02x
+
+//------------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------------
+
+void Config_GPIO();
+
+void Pulse_P20(); // Génération d'une impulsion de 500ns sur P2.0 (Poussoir A)
+
+void Pulse_P21(); // Génération d'une impulsion de 500ns sur P2.1 (Poussoir B)
+
+void Demo_GPIO();
