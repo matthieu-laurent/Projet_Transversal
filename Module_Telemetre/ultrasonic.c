c88872adf4e72b4bc2ca53fdef5f3abd2f589c3c
stancpe <stan_renia@hotmail.com>
2015-03-19 18:36:55 +0100

    Update ultrasonic.c

--- Module_Telemetre/ultrasonic.c
+++ Module_Telemetre/ultrasonic.c
@@ -9,83 +9,88 @@
 #define ULTRASON_TMIN  1*ULTRASON_COEF*ULTRASON_DMIN	// durée minimale mesurée pour laquelle la mesure est correcte (2 cm)
 #define ULTRASON_TMAX  1*ULTRASON_COEF*ULTRASON_DMAX	// durée maximale mesurée pour laquelle la mesure est correcte (400 cm)
 
-sfr16 DPTR=0x82;
 sbit trig=P3^4; // P2.0 en d:0xa0
 sbit INT0=P3^5; // crossbar à faire avec wizard ECHO
+int nb_debordement = 0;
 // **** fin ultrason.h **************************
 
-/*
-P2MDOUT   = 0x7B; // Configuration des bits du port 2
-P3MDOUT   = 0xFC; // Configuration des bits du port 3
-	
-Les 3 lignes suivantes permettent d'activer le crossbar et de router INT1 sur P1.2
-	  XBR0      = 0x07;
-    XBR1      = 0x10;
-		
-EX0 = 1;		
-*/
+
 void envoi_pulsation(void) //fonction permettant de générer un delai de 10us pour declencher le trigger
 {
 	int i;
 	trig=1;
-	for(i=0;i<223;i++) // Pour le delai de 10ms
+	for(i=0;i<223;i++) // Pour le delai de 10us
 	{
-		_nop_(); //1
+		_nop_(); 
 	}
-	/*_nop_(); //2
-	_nop_(); //3
-	_nop_(); //4
-	_nop_(); //5
-	_nop_(); //6
-	_nop_(); //7
-	_nop_(); //8
-	_nop_(); //9
-	_nop_(); //10*/
+
 	trig=0;
+}
+
+void  ISR_Timer0 (void) interrupt 1	
+{
+	//	TF0 cleared by hardware (voir table des interrutptions): Se met à 0 automatiquement 
+	nb_debordement++;	// Compte le nombre de débordements pour Ultrason
 }
 
 unsigned int MES_Dist_AV(void)
 {
+	/* *** Tâche: Mesure la distance entre robot et obstacle, par le capteur avant. ***
+	Fonctionnement Matériel:
+		Envoyer une pulsation de 10µs au capteur, celui-ci renvoie un signal (Echo) d'une durée proportionnelle
+		à la distance entre le capteur et l'obstacle.
+		Calcul: Distance(cm) = Temps_mesuré(µs)/58. 
+	****************************************
+	Explication de la fonction:
+			Envoie une pulsation au capteur (Trigger), via envoi_pulsation()
+			Attend la récéption du signal réponse du capteur (Echo).
+			Initialise à 0 et Démarre le Timer0. ** Voir si utiliser GATE0 (pour démarrer quand TR0=1 et INT0=1) améliore la précision.
+			Attend la fin du signal Echo.
+			Récupère la valeur du timer dans "a". 
+				Afin de convertir "a" en µs: 
+					Prend en compte les débordements du Timer0.
+					Divise "a" par 22 :  Sysclock (22 Mhz)= 22*clock_1µs (1 Mhz).
+			Calcule la distance à renvoyer:
+				Si distance conforme (entre 2cm et 400cm) on renvoie a/58 afin de convertir en cm.
+				Sinon on renvoie 0.
+*********************************************************************		*/
+	
 	unsigned int distance;
-	unsigned int a;
-	int tmp=0;
-	unsigned int usmax = ULTRASON_TMAX;
+	unsigned int a;	// **********************!!!!!!!!!!!!!!!!! CHANGER TYPE mettre sur 32 bits! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//unsigned int usmax = ULTRASON_TMAX; tester sans usmax d'abord
+	
+	nb_debordement = 0;
+	
 	envoi_pulsation();
 
-	while(!INT0); //permet de generer un delai de 40ms
-	TH0=0x00; // mise à 0 Timer0, démarrage mesure durée impulsion Echo du capteur ultrason.
-	TL0=0x00; // TL0 en D:0x8a et TH0 en D:0x8c
-	TR0=1;
-	while(INT0) // INT0 = P2 en 0xA0
-	{
-		//DPH=TH0;
-		
-		//DPL=TL0;
-		if (TH0==0xFF && TL0==0xFF)
-		{
-			tmp++;
-		}
-	}
-	TR0=0;
+	while(!INT0); //	 Attend le signal Echo
+	TH0=0x00; // Mise à 0 Timer0
+	TL0=0x00; // Rq: TL0 en D:0x8a et TH0 en D:0x8c
+	TR0=1; // Démarre la mesure de durée d'impulsion Echo du capteur ultrason.
+	
+	while(INT0);	// Attend fin signal Echo
+	
+	TR0=0; // Arrêt du Timer0
 	a=0;
-	a=TH0;
+	a=TH0;	// Récupère la valeur du Timer0, ces 3 lignes fonctionnent bien.
 	a=a<<8;
 	a|=TL0;
-	if(tmp>0)
+	
+	if(nb_debordement>0)
 	{
-		a=a*tmp*65535;
+		a=a+nb_debordement*65535; // Prend en compte les débordements , déborde dans ISR_Timer0()
 	}
-	a=a/22;
+	a=a/22;	// Convertie en µs : Sysclock (22 Mhz)= 22*clock_1µs (1 Mhz)
 
 	if(a>=ULTRASON_TMIN)  // Si durée mesurée correcte, obstacle > 2cm
 	{
-		if(a<=usmax) // Si durée mesurée correcte, obstacle < 400cm
+		if(a<=ULTRASON_TMAX) // Si durée mesurée correcte, obstacle < 400cm
 		{
 			distance= a/58; // calcule la distance mesurée
 		}
 		else // sinon, obstacle > 400cm
 		{
-			distance = 0; // distance = 400 cm : on peut avancer de 400 cm. A changer peut etre plus tard
+			distance = 0; // Imposé par le cahier des charges
 		}
 	}
 	else
@@ -111,15 +116,14 @@ void main()
 	unsigned int distance_cible=0;
 	
 	Init_Device();
-							// Calcul distance incorrect ********************* revoir timer
+	
 	// Init Timer 0
-	TMOD=0x01; // timer0 en mode 2 (16bit Counter/timer)
-	//TMOD&=0xF9; // timer0 en mode 2 (16bit Counter/timer)
+	TMOD=0x01; // timer0 en mode 1 (16bit Counter/timer)
 	CKCON|=0x08; // system clock
-	IT0 = 1; // INT0 is edge triggered
-	TR0=1; // mise en marche du timer0
+	// IT0 = 1; // INT0 is edge triggered
+	ET0 = 1;	// Active Interruption Timer0
 	
-	
+
 	while(1) // boucle de test
 	{
 		distance_cible=MES_Dist_AV();
