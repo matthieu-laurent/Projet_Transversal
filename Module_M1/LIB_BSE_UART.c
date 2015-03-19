2a9e53b611e6794d7cb1e3394ecb06c50260b693
benadada <benadada.adam@hotmail.fr>
2015-03-19 12:10:41 +0100

    Update LIB_BSE_UART.c

--- Module_M1/LIB_BSE_UART.c
+++ Module_M1/LIB_BSE_UART.c
@@ -148,6 +148,10 @@ void Demo_UART()
 void Analyse_String(char* str)
 {
 	char *p = str;
+	int vitesse = 0;
+	int angle = 0;
+	int coordX = 0;
+	int coordY = 0;
 	
 	while(*p != '\r')
 	{
@@ -160,27 +164,35 @@ void Analyse_String(char* str)
 				{
 					case '1':
 						out.Etat_Epreuve = epreuve1;
+						p++;
 						break;
 					case '2':
 						out.Etat_Epreuve = epreuve2;
+						p++;
 						break;
 					case '3':
 						out.Etat_Epreuve = epreuve3;
+						p++;
 						break;
 					case '4':
 						out.Etat_Epreuve = epreuve4;
+						p++;
 						break;
 					case '5':
 						out.Etat_Epreuve = epreuve5;
+						p++;
 						break;
 					case '6':
 						out.Etat_Epreuve = epreuve6;
+						p++;
 						break;
 					case '8':
 						out.Etat_Epreuve = epreuve8;
+						p++;
 						break;
 					default:
-						out.Etat_Epreuve = Epreuve_non;
+						out.Etat_Epreuve = epreuve1;
+						p++;
 						break;
 				}
 				break;
@@ -194,38 +206,16 @@ void Analyse_String(char* str)
 				p++;
 				if(*p == 'V')
 				{
-					char vitesse[4];
 					p++;
-					if(*p >= '0' && *p <= '9')
+					vitesse = calculVitesse(p);
+					if(vitesse != 0)
 					{
-						vitesse[0] = *p;
+						out.Vitesse = vitesse;
 					}
 					else
 					{
+						out.Vitesse = 0;
 						break;
-					}
-					p++;
-					if(*p >= '0' && *p <= '9')
-					{
-						vitesse[1] = *p;
-					}
-					else
-					{
-						break;
-					}
-					p++;
-					if(*p >= '0' && *p <= '9') // Vitesse à 3 chiffres
-					{
-						vitesse[2] = *p;
-						vitesse[3] = '\0';
-						if(atoi(vitesse) == 100) out.Vitesse = vitesse; 
-						break;
-					}
-					else // Vitesse à 2 chiffres
-					{
-						vitesse[2] = '\0';
-						if(atoi(vitesse) < 100 && atoi(vitesse) >= 10) out.Vitesse = vitesse;
-						break;						
 					}
 				}
 				else
@@ -233,8 +223,250 @@ void Analyse_String(char* str)
 					break;
 				}
 			case 'A':
-				break;
+				p++;
+				if(*p == '\r') // Pas de paramètre
+				{
+					out.Vitesse = 20;
+					out.Etat_Mouvement = Avancer;
+				}
+				
+				if(*p != 'S')
+				{
 					
-		}
+					vitesse = calculVitesse(p);
+					if(vitesse != 0)
+					{
+						out.Vitesse = vitesse;
+						out.Etat_Mouvement = Avancer;
+					}
+					else
+					{
+						out.Etat_Mouvement = Mouvement_non;
+					}
+					break;
+				}
+				if(*p == 'S')
+					// ASS
+				break;
+				
+			case 'B':
+				p++;
+				if(*p == '\r') // Pas de paramètre
+				{
+					out.Vitesse = 20;
+					out.Etat_Mouvement = Reculer;
+				}
+				
+				vitesse = calculVitesse(p);
+				if(vitesse != 0)
+				{
+					out.Vitesse = vitesse;
+					out.Etat_Mouvement = Reculer;
+				}
+				else
+				{
+					out.Etat_Mouvement = Mouvement_non;
+				}
+				break;
+			
+			case 'S':
+				p++;
+				if(*p == '\r')
+				{
+					out.Etat_Mouvement = Stopper;
+				}
+			case 'R':
+				p++;
+				if(*p == 'D') // Rotation à droite de 90°
+				{
+					p++;
+					if(*p == '\r')
+						out.Etat_Mouvement = Rot_90D;
+				}
+				else if(*p == 'G') // Rotation à gauche de 90°
+				{
+					p++;
+					if(*p == '\r')
+						out.Etat_Mouvement = Rot_90G;
+				}
+				else if (*p == 'C') // Rotation complète de la base de 180°
+				{
+					p++;
+					if(*p == '\r')
+						out.Etat_Mouvement = Rot_180D;
+					else
+					{
+						p++;
+						if(*p == 'D')
+							out.Etat_Mouvement = Rot_180D;
+						else if(*p == 'G')
+							out.Etat_Mouvement = Rot_180G;
+					}
+				}
+				else if (*p == 'A')
+				{
+					p++;
+					if(*p == ' ')
+					{
+						p++;
+						if(*p == 'D')
+						{
+							p++;
+							if(*p == ':')
+							{
+								p++;
+								angle = calculAngle(p);
+								if(angle >= 0 && angle <= 180)
+								{
+									out.Angle = angle;
+									out.Etat_Mouvement = Rot_AngD;
+								}
+							}
+						}
+						else if(*p == 'G')
+						{
+							p++;
+							if(*p == ':')
+							{
+								p++;
+								angle = calculAngle(p);
+								if(angle >= 0 && angle <= 180)
+								{
+									out.Angle = angle;
+									out.Etat_Mouvement = RotAngG;
+								}
+							}
+						}
+						else
+						{
+							out.Etat_Mouvement = Mouvement_non;
+						}
+						
+					}
+					else if (*p == '\r') // Pas de paramètres donc cas par défaut
+					{
+						out.Etat_Mouvement = Rot_90D;
+					}
+					break;
+				}
+				break;
+				
+			case 'G':
+				p++;
+				if(*p == ' ')
+				{
+					if(*p == 'X')
+					{
+						p++;
+						if(*p == ':')
+						{
+							p++;
+							p = calculCoord(p,'X');
+						}
+						p++;
+					}
+					if(*p == 'Y')
+					{
+						p++;
+						if(*p == ':')
+						{
+							p++;
+							p = calculCoord(p,'Y');
+						}
+						p++;
+					}
+					if(*p == 'A')
+					{
+						p++;
+						if(*p == ':')
+						{
+							p++;
+							out.Angle = calculAngle(p);
+						}
+					}
+				} // fin case G
+				break;
+			} // fin switch
+	} // while
+} // fin fonction
+
+int calculVitesse(char *str)
+{
+	char *p = str;
+	
+	char vitesse[5];
+	
+	int i = 0;
+	
+	while(*p >= '0' && *p <= '9' && *p != '\0')
+	{
+		vitesse[i] = *p;
+		i++;
+		p++;
 	}
+	vitesse[i]='\0';
+					
+	if(vitesse >= 10 && atoi(vitesse) <= 100)
+		return atoi(vitesse);
+	else
+		return 0;
+}
+
+int calculAngle (char* str)
+{
+	char *p = str;
+	
+	char angle[6];
+	
+	int i = 0;
+	
+	while((*p >= '0' && *p <= '9') || *p=='-')
+	{
+		angle[i] = *p;
+		i++;
+		p++;
+	}
+	angle[i]='\0';
+					
+	if(atoi(angle) >= -180 && atoi(angle) <= 180)
+		return atoi(angle);
+	else
+		return 0;
+
+}
+
+char* calculCoord(char* str, char c)
+{
+	char *p =str;
+	
+	char coord[4];
+	
+	int i = 0;
+	
+	while((*p >='0' && *p <='9') || *p == '-')
+	{
+		coord[i]= *p;
+		i++;
+		p++;
+	}
+	coord[i] = '\0';
+	
+	if(c == 'X')
+	{
+		if(atoi(coord) >= - 99 && atoi(coord) <= 99)
+			out.Coord_X = atoi(coord);
+		else
+			out.Coord_X = 0;
+	}
+	else if(c == 'Y')
+	{
+		if(atoi(coord) >= - 99 && atoi(coord) <= 99)
+			out.Coord_Y = atoi(coord);
+		else
+			out.Coord_Y = 0;
+	}
+	
+	
+	return p;
+	
 }
