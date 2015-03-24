d142ebc4cc2892f32222c089bbf1150f3d0e64d1
pierrickcpe <pv35@free.fr>
2015-03-24 16:29:25 +0100

    Update LIB_BSE_UART.h

--- M2/LIB_BSE_UART.h
+++ M2/LIB_BSE_UART.h
@@ -1,4 +1,7 @@
 
+//*****************************************************************************************************************************************
+// Fonction de l'UART1
+//*****************************************************************************************************************************************
 
 void CFG_Clock_UART1(void);
 void CFG_UART1(void);
@@ -6,8 +9,13 @@ char Putchar(char c, char csg_tempo);
 char Send_String(char *char_ptr);
 char Getchar(void);
 void Action_UART1();
+
+//*****************************************************************************************************************************************
+// Fonction du Serializer
+//*****************************************************************************************************************************************
+
 void Init_Commande_Serializer();
-void Avancer(int vitesse);
+void Avancer(int vitesse); //vitesse=50 => 50% de la vitesse
 void Reculer(int vitesse);
 void Arret();
 void Rotation_Droite_90(int vitesse);
