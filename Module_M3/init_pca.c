8b6102e511da61c1efbca2b0aa03d510cabfa340
LeoSchmitt <leo.schmitt@cpe.fr>
2015-03-31 17:50:40 +0200

    Update init_pca.c

--- Module_M3/init_pca.c
+++ Module_M3/init_pca.c
@@ -2,28 +2,22 @@
 
 void PCA_Init(void)
 {
-		CF 				= 0; // remise à zéro du flag
-    EIE1 			+= 0x08; // enable pca interrupt
     PCA0MD    = 0x81; // suspend PCA opeartion while system controller is in Idle Mode & enable interrupt
-		PCA0L     = 0xFF;
-    PCA0H     = 0x00; // Permet d'avoir une fréquence de 50Hz
-    // Pour le module  0
-		PCA0CPM0  = 0xC2; // Module0 in 16-bit Pulse Width Modulator
-    PCA0CPL0  = 0xCC;
-    PCA0CPH0  = 0xCC; // Permet d'avoir un rapport cyclique de 20%
-		// Pour le module 1
-	  PCA0CPM1  = 0xC2; // Module1 in 16-bit Pulse Width Modulator
-    PCA0CPL1  = 0xCC; 
-    PCA0CPH1  = 0xCC; // Permet d'avoir un rapport cyclique de 20%
-		// Pour le module 2
-	  PCA0CPM2  = 0xC2; // Module1 in 16-bit Pulse Width Modulator
-    PCA0CPL2  = 0xCC;
-    PCA0CPH2  = 0xCC; // Permet d'avoir un rapport cyclique de 20%
-		// Pour le module 3
-
-		// Pour le module 4
-	
-		PCA0CN    = 0x40; // enable PCA
+    PCA0CPM0  = 0xC2; // Module0 in 16-bit Pulse Width Modulator
+    PCA0CPM1  = 0xC2; // Module1 in 16-bit Pulse Width Modulator
+    PCA0CPM2  = 0xC2; // Module2 in 16-bit Pulse Width Modulator
+    PCA0L     = 0x07; // Permet d'avoir une fréquence de 50Hz
+		PCA0H     = 0x70; // Permet d'avoir une fréquence de 50Hz
+    
+		PCA0CPL0  = 0xFD; // Permet d'initialiser le servo 0 à la position 0°
+    PCA0CPH0  = 0xF4; // Permet d'initialiser le servo 0 à la position 0°
+		PCA0CPL1  = 0xFD; // Permet d'initialiser le servo 1 à la position 0°
+    PCA0CPH1  = 0xF4; // Permet d'initialiser le servo 1 à la position 0°
+		PCA0CPL2  = 0xFD; // Permet d'initialiser le servo 2 à la position 0°
+		PCA0CPH2  = 0xF4; // Permet d'initialiser le servo 2 à la position 0°
+    
+		PCA0CN    = 0x40;
+    EIE1      = 0x08; // enable pca interrupt
 }
 
 void PCA_ISR(void) interrupt 9
