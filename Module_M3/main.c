b6439e22908df93e251580224ef5f474659424d4
LeoSchmitt <leo.schmitt@cpe.fr>
2015-04-02 09:57:50 +0200

    Update main.c

--- Module_M3/main.c
+++ Module_M3/main.c
@@ -13,10 +13,15 @@
 
 #ifndef config_global
 #define config_global
-#include <config_global.h>
+#include "config_global.h"
 #endif				// Configuration globales du µprocesseur
-#include <init_pca.h>
-#include <fonctions_servo.h>
+#include "init_pca.h"
+#include "fonctions_servo.h"
+
+#ifndef FO_M1__STRUCTURES_COMMANDES_INFORMATIONS_CENTRALEDECOMMANDE_H
+#define FO_M1__STRUCTURES_COMMANDES_INFORMATIONS_CENTRALEDECOMMANDE_H
+#include "FO_M1__Structures_COMMANDES_INFORMATIONS_CentraleDeCommande.h"
+#endif
 
 sbit P14=P1^4;
 //------------------------------------------------------------------------------------
@@ -36,17 +41,14 @@ void main (void) {
 	PCA_Init();
 	EA=1; // Autorisation interruptions
 
-
-
+//	cmd_c.Etat_Servo=Servo_V;
+//	cmd_c.Servo_Angle=-90;
+	
 	while (1) { // Boucle infinie
-		
-		for(i=0;i<500000;i++);
-		CDE_Servo_V(Angle);
-		Angle=Angle+10;
-		if(Angle==90)
-		{
-			Angle=-90;
-		}
-		
+			CDE_Servo();
+			/*for(i=0;i<50000;i++)
+			cmd_c.Servo_Angle=cmd_c.Servo_Angle+5;
+			if(cmd_c.Servo_Angle==90)
+				cmd_c.Servo_Angle=-90;*/
 	}
 }
