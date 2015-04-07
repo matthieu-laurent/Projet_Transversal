968d7931561f72f8ecb4f747549f3c54818e4263
LeoSchmitt <leo.schmitt@cpe.fr>
2015-04-07 17:53:46 +0200

    Create init_pca.c

---
+++ Module_FO_S2/init_pca.c
@@ -0,0 +1,19 @@
+#include "init_pca.h"
+
+void PCA_Init(void)
+{
+    PCA0MD    = 0x80; // suspend PCA opeartion while system controller is in Idle Mode & enable interrupt
+    PCA0CPM4  = 0xC2; // Module4 in 16-bit Pulse Width Modulator
+    
+    PCA0L     = 0x00; // Permet d'avoir une fréquence de 65536*sysclk/12
+		PCA0H     = 0x00; // Permet d'avoir une fréquence de 65536*sysclk/12
+    
+		PCA0CPL4  = 0x00; // Permet d'avoir un rapport cyclique de 0% => par défaut la LED doit être éteinte 
+    PCA0CPH4  = 0x80; // Permet d'avoir un rapport cyclique de 0%
+    
+		PCA0CN    = 0x40;
+}
+
+
+
+
