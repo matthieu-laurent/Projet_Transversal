6bd7691cff8b751258e13176934eb5bcb884b35f
LeoSchmitt <leo.schmitt@cpe.fr>
2015-04-07 17:52:51 +0200

    Create laser.c

---
+++ Module_FO_S2/laser.c
@@ -0,0 +1,47 @@
+#include "laser.h"
+
+#ifndef config_global
+#define config_global
+#include "config_global.h"
+#endif	
+
+#ifndef configTimer3Slave
+#define configTimer3Slave
+#include "configTimer3Slave.h"
+#endif
+/*
+Intensité: intensité lumineuse exprimée en pourcentage de luminosité
+
+Lum_ON: Durée d’allumage exprimée en dixièmes de seconde (de 1 à 100)
+
+Lum_OFF: Durée d’extinction exprimée en dixièmes de seconde (de 1 à 100)
+
+Lum_OFF: Nombre de cycles d’allumage-Extinction (de 1 à 100) 
+*/
+
+/* Variables globales*/
+	extern unsigned char Intensite;
+	extern unsigned char Lum_ON;
+	extern unsigned char Lum_OFF;
+	extern unsigned char Lum_Nbre;
+	extern unsigned char temps;
+/******************************/
+
+void Lumiere (void) //unsigned char Intensite, unsigned char Lum_ON, unsigned char Lum_OFF, unsigned char Lum_Nbre) 
+{
+	temps=Lum_ON*12/SYSCLK;
+	Lum_Nbre*=2;
+	configTimer3();
+	while(Lum_Nbre)
+	{
+	}
+	
+}
+
+/*
+Extinction inconditionnelle de la source lumineuse
+*/
+void Lumiere_Stop (void)
+{
+	
+}
