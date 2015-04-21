cb99e5e5bea48a8417d2298cb090166433ec20c6
stancpe <stan_renia@hotmail.com>
2015-04-21 16:34:08 +0200

    Update Gestion_Globale_Master.h

--- Module_M5/Gestion_Globale_Master.h
+++ Module_M5/Gestion_Globale_Master.h
@@ -1,6 +1,27 @@
 
 
-#define CMD_OK 1 
+#include "LIB_BSE_Config_Global.h"
+#include "commandes_info_centrale.h"
+#include "commandes_info_serializer.h"
+#include "Ring_buffer.h"
+#include <intrins.h>
+#include <stdio.h>
+#include "Controle_Moteur.h"
+#include "Module_M1.h"
+#include "servo.h"
 
-typedef unsigned char BYTE;
-typedef char SIGNED_BYTE;
+#define CMD_OK 1 
+#define COEF_NS_US 22 //0.000001/(1/SYSCLK);	// coefficient pour convertir 1 sysclk(env. 45ns) en 1 us
+
+
+
+void UART0_ISR(void);
+void UART1_ISR(void);
+
+void Gestion_Mouvement(void);
+void Gestion_DCT_Obst(void);
+void Gestion_Servo(void);
+void Gestion_Lumiere(void);
+
+unsigned int MES_Dist_AV(void);
+void delai_us(unsigned long int duree);
