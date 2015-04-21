b28cb37adc9ba5d35d2c5d7755866303fe491050
stancpe <stan_renia@hotmail.com>
2015-04-21 16:35:08 +0200

    Create LIB_BSE_UART.h

---
+++ Module_M5/LIB_BSE_UART.h
@@ -0,0 +1,9 @@
+void CFG_Clock_UART0(void);
+void CFG_UART0(void);
+char Putchar_U0(char c, char csg_tempo);
+char Send_String_U0(char *char_ptr);
+char Getchar_U0(void);
+
+char Send_String(char *char_ptr);
+char Putchar(char c);
+char Getchar(void);
