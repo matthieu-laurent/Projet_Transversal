e38e261900591364df0d1df55ce4d18d1c4742c3
stancpe <stan_renia@hotmail.com>
2015-04-23 12:02:24 +0200

    Update v.0_41

--- Module_M5/LIB_BSE_GPIO.c
+++ Module_M5/LIB_BSE_GPIO.c
@@ -3,7 +3,7 @@
 
 void Config_GPIO(void)
 {
-	// P0.0  -  Tx UART0
+	// P0.0  -  Tx UART0, PushPull
 	// P0.1  -  Rx UART0
 	// P0.2  -  Unassigned,  Open-Drain, Digital
 	// P0.3  -  Unassigned,  Open-Drain, Digital
@@ -12,13 +12,13 @@ void Config_GPIO(void)
 	// P0.6  -  Unassigned,  Open-Drain, Digital
 	// P0.7  -  Unassigned,  Open-Drain, Digital
 
-	// P1.0  -  Tx UART1
+	// P1.0  -  Tx UART1, PushPull
 	// P1.1  -  Rx UART1
-	// P1.2  -  Fin_PES,  Open-Drain, Digital (int1)
-	// P1.3  -  Unassigned,  Open-Drain, Digital
-	// P1.4  -  Unassigned,  Open-Drain, Digital
+	// P1.2  -  CEX0, PushPull
+	// P1.3  -  CEX1, PushPull
+	// P1.4  -  CEX2, PushPull
 	// P1.5  -  Unassigned,  Open-Drain, Digital
-	// P1.6  -  Unassigned,  Open-Drain, Digital
+	// P1.6  -  LED Verte - NE PAS UTILISER CE PORT POUR AUTRE CHOSE
 	// P1.7  -  Unassigned,  Open-Drain, Digital
 
 	// P2.0  -  Decl_PP1,  Push-Pull,  Digital
@@ -36,20 +36,22 @@ void Config_GPIO(void)
 	// P3.3  -  Decl_PES,  Push-Pull,  Digital
 	// P3.4  -  Trig UltraSon
 	// P3.5  -  Echo Ultrason
-	// P3.6  -  Tst6,  Push-Pull,  Digital
-	// P3.7  -  Tst7,  Open-Drain, Digital
+	// P3.6  -  Trig UltraSon_AR
+	// P3.7  -  Echo Ultrason_AR
 
-		P1MDOUT   = 0x01;
-    P3MDOUT   = 0x10;
+		P1MDOUT   = 0x1D;
+    P3MDOUT   = 0x50;
    /* XBR0      = 0xEF;
     XBR1      = 0xFF;
     XBR2      = 0x5D;*/
 		
 		//test moteur
-		XBR0      = 0x07;
+		//XBR0      = 0x07;
+		 XBR0      = 0x1F;
     XBR2      = 0x44;
 		
 		trig_ultrason = 0;
+		trig_ultrason_AR = 0;
 }
 
 void Demo_GPIO(void)
