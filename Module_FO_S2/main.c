804b6b6c91ff913f2ffa2fcf1920f0ce0120d2aa
LeoSchmitt <leo.schmitt@cpe.fr>
2015-04-23 12:08:43 +0200

    Update main.c

--- Module_FO_S2/main.c
+++ Module_FO_S2/main.c
@@ -26,23 +26,28 @@ unsigned char Intensite;
 unsigned char Lum_ON;
 unsigned char Lum_OFF; 
 unsigned char Lum_Nbre;
-unsigned char temps;
-
+unsigned int cpt;
+unsigned int nbOverflow;
+unsigned char ON;
 
 //------------------------------------------------------------------------------------
 // MAIN Routine
 //------------------------------------------------------------------------------------
 void main (void) {
-	Intensite=80;
-	Lum_ON=50;// choisir valeur entre 1 et 100
-	Lum_OFF=40;// choisir valeur entre 1 et 100 !!!! attention prendre différent de Lum_ON
-	Lum_Nbre=10;
-	temps=Lum_ON*10*12/SYSCLK;
+
+	
+	Intensite=80; // en %
+	Lum_ON=20;// choisir valeur entre 1 et 100
+	Lum_OFF=10;// choisir valeur entre 1 et 100 
+	Lum_Nbre=10; 
+	cpt=0;
+	ON=1;
+	nbOverflow=Lum_ON*4; // (x*10^-1)/(25*10^-3) rappel: un overflow tt les 25 ms
 	
 	Init_Device();
 	PCA_Init();
 	EA=1; // Autorisation interruptions
-
+	
 	Lumiere();
 	while (1) { // Boucle infinie
 	
