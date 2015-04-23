2c5c51325bbe442138af5ce2d7337548df1a1eaf
stancpe <stan_renia@hotmail.com>
2015-04-23 12:03:05 +0200

    Update v.0_41

--- Module_M5/Ring_buffer.c
+++ Module_M5/Ring_buffer.c
@@ -1,107 +1,31 @@
-#include <c8051f020.h>                    // SFR declarations
+                    // SFR declarations
 #include <stdio.h>
+#include <stdlib.h>
 #include <intrins.h>
 #include <string.h>
-//*************************************************************************************************
-//*************************************************************************************************
-// GESTION DE BUFFER CIRCULAIRE
-//
-// Code tir� du document Powerpoint: Microcontroller Programming III	MP7-46
-//
-//*************************************************************************************************
-
-//*************************************************************************************************
-// DEFINITION DES MACROS DE GESTION DE BUFFER CIRCULAIRE
-//*************************************************************************************************
-
-// Structure de gestion de buffer circulaire
-	//rb_start: pointeur sur l'adresse de d�but du buffer 
-	// rb_end: pointeur sur l'adresse de fin du buffer	
-	// rb_in: pointeur sur la donn�e � lire
-	// rb_out: pointeur sur la case � �crire
-		
-#define RB_CREATE(rb, type) \
-   struct { \
-     type *rb_start; \	   
-     type *rb_end; \	   
-     type *rb_in; \
-	 type *rb_out; \		
-	  } rb
-
-//Initialisation de la structure de pointeurs 
-// rb: adresse de la structure
-// start: adresse du premier �l�ment du buffer 
-// number: nombre d'�l�ments du buffer - 1	(le "-1" n'est � mon avis pas n�cessaire)
-#define RB_INIT(rb, start, number) \
-         ( (rb)->rb_in = (rb)->rb_out= (rb)->rb_start= start, \
-           (rb)->rb_end = &(rb)->rb_start[number] )
-
-//Cette macro rend le buffer circulaire. Quand on atteint la fin, on retourne au d�but
-#define RB_SLOT(rb, slot) \
-         ( (slot)==(rb)->rb_end? (rb)->rb_start: (slot) )
-
-// Test: Buffer vide? 
-#define RB_EMPTY(rb) ( (rb)->rb_in==(rb)->rb_out )
-
-// Test: Buffer plein?
-#define RB_FULL(rb)  ( RB_SLOT(rb, (rb)->rb_in+1)==(rb)->rb_out )
-
-// Incrementation du pointeur dur la case � �crire
-#define RB_PUSHADVANCE(rb) ( (rb)->rb_in= RB_SLOT((rb), (rb)->rb_in+1) )
-
-// Incr�mentation du pointeur sur la case � lire
-#define RB_POPADVANCE(rb)  ( (rb)->rb_out= RB_SLOT((rb), (rb)->rb_out+1) )
-
-// Pointeur pour stocker une valeur dans le buffer
-#define RB_PUSHSLOT(rb) ( (rb)->rb_in )
-
-// pointeur pour lire une valeur dans le buffer
-#define RB_POPSLOT(rb)  ( (rb)->rb_out )
-
-
-//*************************************************************************************************
-
-#define       MAX_BUFLEN 32
-#define       MY_BUF	 10
-static char  xdata outbuf[MAX_BUFLEN];     /* memory for ring buffer #1 (TXD) */
-static char  xdata inbuf [MAX_BUFLEN];     /* memory for ring buffer #2 (RXD) */
-static  bit   TXactive = 0;             /* transmission status flag (off) */
-
-/* define o/p and i/p ring buffer control structures */
-static RB_CREATE(out,unsigned char xdata);            /* static struct { ... } out; */
-static RB_CREATE(in, unsigned char xdata);            /* static struct { ... } in; */
+#include "LIB_BSE_Config_Global.h"
+#include "Ring_buffer.h"
 
 //**************************************************************************************************
 //**************************************************************************************************
-void UART0_ISR(void) interrupt 0x4 {
 
-  if (TI0==1) // On peut envoyer une nouvelle donn�e sur la liaison s�rie
-  { 
-  	if(!RB_EMPTY(&out)) {
-       SBUF1 = *RB_POPSLOT(&out);      /* start transmission of next byte */
-       RB_POPADVANCE(&out);             /* remove the sent byte from buffer */
-  	}
-  	else TXactive = 0;                    /* TX finished, interface inactive */
-	TI0 = 0;
-  }
-  else // RI0 à 1 - Donc une donn�e a �t� re�ue
-  {
-    if(!RB_FULL(&in)) {
-     	*RB_PUSHSLOT(&in) = SBUF1;        /* store new data in the buffer */
-		RB_PUSHADVANCE(&in);               /* next write location */
-	}
-   RI0 = 0;
-  }
-}
 //**************************************************************************************************
-void serInit(void) {
+/*void serInit(void) {
 
-  RB_INIT(&out, outbuf, MAX_BUFLEN-1);           /* set up TX ring buffer */
-  RB_INIT(&in, inbuf,MAX_BUFLEN-1);             /* set up RX ring buffer */
+  RB_INIT(&out, outbuf, MAX_BUFLEN-1);           // set up TX ring buffer 
+  RB_INIT(&in, inbuf,MAX_BUFLEN-1);             // set up RX ring buffer 
 
 	// Ajouter ici le code de l'initialisation et de configuration de l'UART0
 
-}
+} */
+
+// Déclaration des variables en "extern" indispensable car  ces variables globales provoquent des erreurs via un header (.h), peut-être dû au compilateur de Keil 
+extern char  xdata outbuf[MAX_BUFLEN];     // memory for ring buffer #1 (TXD) 
+extern char  xdata inbuf [MAX_BUFLEN];     // memory for ring buffer #2 (RXD) 
+extern RB_CREATE(out,unsigned char xdata);            // static struct { ... } out; 
+extern RB_CREATE(in, unsigned char xdata);            // static struct { ... } in; 
+extern bit TXactive;
+extern char xdata *msg_info;
 // **************************************************************************************************
 // SerOutchar: envoi d'un caract�re dans le buffer de transmission de la liaison s�rie
 // *************************************************************************************************
@@ -112,9 +36,12 @@ unsigned char serOutchar(char c) {
   	*RB_PUSHSLOT(&out) = c;               /* store data in the buffer */
   	RB_PUSHADVANCE(&out);                 /* adjust write position */
 
+		 /* On place toujours le char dans le buffer (si pas plein), TXactive déclenche la transmission 
+			et repasse à zéro quand la transmission est terminée (dernier char envoyé, c-a-dire, buffer vide) */
+
   	if(!TXactive) {
 		TXactive = 1;                      /* indicate ongoing transmission */
- 	    TI0 = 1;//   Placer le bit TI � 1 pour provoquer le d�clenchement de l'interruption
+ 	    TI0 = 1;//   Placer le bit TI à 1 pour provoquer le d�clenchement de l'interruption
   	}
 		else return 0;
 	return 1;  // op�ration correctement r�alis�e 
@@ -155,10 +82,21 @@ char c;
 // serOutstring:  Envoi d'une chaine de caract�re dans le buffer de transmission
 // ************************************************************************************************
 unsigned char serOutstring(char *buf) {
-unsigned char len,code_err=0;
+unsigned char len,lenght,code_err=0;
 
-  for(len = 0; len < strlen(buf); len++)
+	lenght = strlen(buf);
+  for(len = 0; len < lenght; len++)	// !!!! NE PAS ECRIRE "len < strlen(buf)" CELA PROVOQUE UN DEBORDEMENT DE 'buf' raison inconnue.
      code_err +=serOutchar(buf[len]);
+	
+  return code_err;
+}
+unsigned char serOutstring_tst(void) {
+unsigned char len,lenght,code_err=0;
+
+	lenght = strlen(msg_info);
+  for(len = 0; len < lenght; len++)	// !!!! NE PAS ECRIRE "len < strlen(buf)" CELA PROVOQUE UN DEBORDEMENT DE 'buf' raison inconnue.
+     code_err +=serOutchar(msg_info[len]);
+	
   return code_err;
 }
 // **************************************************************************************************
@@ -167,31 +105,69 @@ unsigned char len,code_err=0;
                      /* size of local string buffer */ 
 sbit bitin = P1^0;
 sbit bitout = P1^1;
+bit start_test=0;	
 static char  userbuf[MY_BUF];
 
 static char  *error = "\n\rError: Buffer overflow.\n\r";
 
-void main(void) {
-//long  x, y, z;
-unsigned char   len, value, rc = 0;
-   WDTCN     = 0xDE;
-    WDTCN     = 0xAD;
+void test_ring_buffer(void) {
+//long  x, y, z; 
+
+unsigned char value, rc = 0, id_cmd=0;
+unsigned char cmd[30] = "";
+	Init_Device();
+	
   serInit();                            
- // EA = 1;                              /* allow interrupts to happen */
-while(1) {
+ // EA = 1;                              // allow interrupts to happen
+	bitin = 0;
+	while(1){
+	
+		if(start_test){
+			start_test = 0;
+			id_cmd = 0;
+		do{
+					value = serInchar();
+					if(value != 0)
+					{
+						cmd[id_cmd] = value;
+						id_cmd++;
+					}
+			}while((value != 0) && (value != '\r'));				
+				
+		if(value == '\r')
+		{			
+			printf("cmd = %s",cmd);
+			// analyse cmd
+		}
+
+		if(value == 0)
+		{
+			//buffer vide : erreur ou ne fait rien
+		}			
+		
+		
+		}
+	/*while(1) {
 
 	  value =serInchar_tst();
 	  if (bitout==1)
 	  {
 	  value=serOutchar('A');
-	  value=serOutchar('B');
-	  value=serOutchar('C');
+	  value=serOutchar(' ');
+	  value=serOutchar('8');
+		value=serOutchar('\r');
 
 	  }
 	  if (bitin==1)
 	  {
 	     value =serInchar_tst();
-	  }
+	  } */
  
-  } /* while */
+  }  /* while */
 } /* main */
+
+void delay(int temps){
+	int i;
+	
+	for(i=0;i<temps;i++){}
+}
