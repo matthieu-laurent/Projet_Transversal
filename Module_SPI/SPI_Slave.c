85f0e726c99bc6e8857dd6a2c3f6cb53ed52f8e7
victorfaramond <victor.faramond@gmail.com>
2015-05-12 13:55:10 +0200

    Update SPI_Slave.c

--- Module_SPI/SPI_Slave.c
+++ Module_SPI/SPI_Slave.c
@@ -1,198 +1,191 @@
-// Module SPI partie Slave
-
-//-----------------------------------------------------------------------------   
-// Includes   
-//----------------------------------------------------------------------------- 
-#include "C8051F020.h"
-//-----------------------------------------------------------------------------   
-// Global Constants   
-//----------------------------------------------------------------------------- 
-#define SYSCLK             16000000    // Internal oscillator frequency in Hz 
-
-#define SPI_CLOCK          250000      // Maximum SPI clock   
-                                       // The SPI clock is a maximum of 250 kHz   
-                                       // when this example is used with   
-                                       // the SPI0_Slave code example. 									   
-//-----------------------------------------------------------------------------   
-// Global Variables   
-//-----------------------------------------------------------------------------   
+/* @section  I N C L U D E S */
+#include <C8051F020.h>
+#include <string.h>
+#include <stdio.h>
+#include <FO_M1__Structures_COMMANDES_INFORMATIONS_CentraleDeCommande.h>
 char serial_data;
-char data_example = 0x55;
+sbit test = P1^0;
+char data_example=0x55;
 char data_save;
-bit transmit_completed = 0;						   
-//-----------------------------------------------------------------------------   
-// Function Prototypes   
-//-----------------------------------------------------------------------------  
-void Reset_Sources_Init(void);
-void Oscillator_Init(void);
-void SPI_Init(void);
-void Port_IO_Init(void);
-void Interrupts_Init(void);
-void Init_Device(void);
-void SPI_Byte_Write (void);   
-void SPI_Byte_Read (void);   
-void SPI_Array_Write (void);   
-void SPI_Array_Read (void);
-//-----------------------------------------------------------------------------  
-// FUNCTION_PURPOSE: This file set up spi in master mode with 
-// Fclk Periph/128 as baud rate and with slave select pin.
-// FUNCTION_INPUTS: P1.5(MISO) serial input
-// FUNCTION_OUTPUTS: P1.7(MOSI) serial output
-//----------------------------------------------------------------------------- 
-void main(void)
+bit transmit_completed= 0;
+char* p;
+char machaine[15];
+
+// Envoit des chaines de la forme "DD" + 0x00 + 0x00 + "FF"
+char* Envoi_Trame_SPI()
 {
-	Init_Device();
-	EA = 1;
-	transmit_completed = 1;
-	serial_data = 0x20;
-
-	while(1)			   			/* endless  */
-   {
-	   if(transmit_completed)
-		{
-		   SPI0DAT = serial_data;       /* echo data to master */
-		   transmit_completed = 0;    /* clear software transfert flag */
-		}
-   }
-
+	char machaine[10];
+	sprintf(machaine, "DD%c%cFF\0", cmd_c.Etat_Lumiere, cmd_c.Etat_ACQ_Son);
+	
+	return machaine;
 }
 
-//-----------------------------------------------------------------------------   
-// Initialization Subroutines   
-//-----------------------------------------------------------------------------   
-   
-//-----------------------------------------------------------------------------   
-// Reset_Sources_Init  
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// This function disables the watchdog timer.   
-//   
-//-----------------------------------------------------------------------------  
+// Renvoie 1 si trame ok, 0 sinon
+// Reçoit des chaines de la forme "DD" + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + "FF"
+int Reception_Trame_SPI(char* str)
+{
+	char *p = str;
+
+	if(*p != 'D')	return 0;
+	if(*p != '\0') p++;
+	if(*p != 'D') return 0;
+	if(*p != '\0') p++;
+	cmd_c.Etat_Lumiere = *p;
+	if(*p != '\0') p++;
+	cmd_c.Lumiere_Intensite = *p;
+	if(*p != '\0') p++;
+	cmd_c.Lumiere_Duree = *p;
+	if(*p != '\0') p++;
+	cmd_c.Lumiere_Extinction = *p;
+	if(*p != '\0') p++;
+	cmd_c.Lumiere_Nbre = *p;
+	if(*p != '\0') p++;
+	cmd_c.Etat_ACQ_Son = *p;
+	if(*p != '\0') p++;
+	cmd_c.ACQ_Duree = *p;
+	if(*p != '\0') p++;
+	if(*p != 'F') return 0;
+	if(*p != '\0') p++;
+	if(*p != 'F') return 0;
+	return 1;
+}
+
 void Reset_Sources_Init()
 {
     WDTCN     = 0xDE;
     WDTCN     = 0xAD;
 }
 
-//-----------------------------------------------------------------------------   
-// Oscillator_Init   
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// This function initializes the system clock to use the external crystal oscillator  
-// at 12 MHz.   
-//   
-//-----------------------------------------------------------------------------  
-void Oscillator_Init()
-{
-    int i = 0;
-    OSCXCN    = 0x67;
-    for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
-    while ((OSCXCN & 0x80) == 0);
-    OSCICN    = 0x08;
-}
-
-//-----------------------------------------------------------------------------   
-// SPI_Init   
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// Configures SPI0 to use 4-wire Slave mode. The SPI timing is   
-// configured for Mode 0,0 (data centered on first edge of clock phase and   
-// SCK line low in idle state).   
-//   
-//----------------------------------------------------------------------------- 
 void SPI_Init()
 {
-    SPI0CN    = 0x01; // 4-wire Slave mode, SPI enabled
+    SPI0CFG   = 0x80;
+    SPI0CN    = 0x05;
+		SPI0CKR = 0x04;
 }
 
-//-----------------------------------------------------------------------------   
-// Port_IO_Init   
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// This function configures the crossbar and GPIO ports.   
-//   
-// P0.0  -  TX0 (UART0), Open-Drain, Digital
-// P0.1  -  RX0 (UART0), Open-Drain, Digital
-// P0.2  -  SCK  (SPI0), Push-Pull,  Digital
-// P0.3  -  MISO (SPI0), Open-Drain, Digital
-// P0.4  -  MOSI (SPI0), Push-Pull,  Digital
-// P0.5  -  NSS  (SPI0), Push-Pull,  Digital 
-//   
-//----------------------------------------------------------------------------- 
 void Port_IO_Init()
 {
-	P0MDOUT   = 0x34;
-    XBR0      = 0x06;
+    // P0.0  -  SCK  (SPI0), Open-Drain, Digital
+    // P0.1  -  MISO (SPI0), Open-Drain, Digital
+    // P0.2  -  MOSI (SPI0), Open-Drain, Digital
+    // P0.3  -  NSS  (SPI0), Open-Drain, Digital
+    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.5  -  Unassigned,  Open-Drain, Digital
+    // P0.6  -  Unassigned,  Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
+
+    // P1.0  -  Unassigned,  Open-Drain, Digital
+    // P1.1  -  Unassigned,  Open-Drain, Digital
+    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Open-Drain, Digital
+    // P3.5  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Open-Drain, Digital
+    // P3.7  -  Unassigned,  Open-Drain, Digital
+
+		P0MDOUT   = 0x02;
+		P1MDOUT = 0x01;
+    XBR0      = 0x02;
     XBR2      = 0x40;
 }
 
-//-----------------------------------------------------------------------------   
-// Interrupts_Init   
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// This function enables the SPI0 interruption.
-//   
-//----------------------------------------------------------------------------
-void Interrupts_Init()
+void Oscillator_Init()
 {
-    IE        = 0x80; // Enable All Interrupts
-    EIE1      = 0x01; // Bit ESPI0 activated (vector 6)
+		int i = 0;
+		OSCXCN    = 0x67;// crystal oscillator mode + f> 6, 7MHz + osc. not yet stable
+		for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
+		while ((OSCXCN & 0x80) == 0); // on attend que l'oscillateur soit stable
+		OSCICN    = 0x0C; // utilisation de l'oscillateur externe
+
 }
 
-//-----------------------------------------------------------------------------   
-// Init_Device   
-//-----------------------------------------------------------------------------   
-//   
-// Return Value : None   
-// Parameters   : None   
-//   
-// Calls all device initialization functions.   
-//   
-//-----------------------------------------------------------------------------  
+void Interrupts_Init()
+{
+    IE        = 0x80;
+    EIE1      = 0x01;
+		EA = 1;
+}
+
+// Initialization function for device,
+// Call Init_Device() from your main program
 void Init_Device(void)
 {
-	Reset_Sources_Init();
+    Reset_Sources_Init();
     SPI_Init();
     Port_IO_Init();
-	Oscillator_Init();
+    Oscillator_Init();
     Interrupts_Init();
 }
 
-//-----------------------------------------------------------------------------
-// FUNCTION_PURPOSE:interrupt
-// FUNCTION_INPUTS: void
-// FUNCTION_OUTPUTS: transmit_complete is software transfert flag
-//-----------------------------------------------------------------------------
-void SPI_ISR(void) interrupt 6 // Adress : 0x0033
+/**
+ * FUNCTION_PURPOSE: This file set up spi in master mode with 
+ * Fclk Periph/128 as baud rate and with slave select pin.
+ * FUNCTION_INPUTS: P1.5(MISO) serial input
+ * FUNCTION_OUTPUTS: P1.7(MOSI) serial output
+ */
+ 
+char tableau[50];
+int index = 0;
+
+void main(void)
 {
-	while(!TXBSY); // On attend la fin 
-	
-	if(RXOVRN) // Overrun case
+	Init_Device();
+
+	while(1)			   			/* endless  */
+   {
+	   if(transmit_completed)
+		 {
+			 //SPI0DAT = serial_data; // echo to master
+			 //while(TXBSY);
+			 transmit_completed = 0;
+		 }
+		 // De 0 à 11 (12 caractères char)
+		 if(index == 12)
+		 {
+			 int verif = Reception_Trame_SPI(tableau);
+			 index = 0;
+		 }
+   }
+
+}
+
+
+/**
+ * FUNCTION_PURPOSE:interrupt
+ * FUNCTION_INPUTS: void
+ * FUNCTION_OUTPUTS: transmit_complete is software transfert flag
+ */
+void it_SPI(void) interrupt 6 
+{
+	while(TXBSY);         /* read and clear spi status register */
+  
+	serial_data=SPI0DAT;   /* read receive data */
+	if(index < 12)
 	{
-		RXOVRN = 0;
-		SPIF = 0;
+		tableau[index] = serial_data; // Stockage
+		index++;
 	}
-	if(SPIF) // Fin d'un transfert
-	{
-		serial_data = SPI0DAT;
-		transmit_completed = 1;
-		SPIF = 0;
-	}
+	transmit_completed=1;/* set software flag */
+	//SPI0DAT = serial_data; // echo data to master
 	
-    SPI0DAT = serial_data;        /* needed to complete clearing sequence */
+	SPIF = 0;
+	
 }
