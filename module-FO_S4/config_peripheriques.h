2aafa8c1fff738a129c39127d57708bbe9e21e8c
LeoSchmitt <leo.schmitt@cpe.fr>
2015-05-12 17:56:03 +0200

    Update config_peripheriques.h

--- module-FO_S4/config_peripheriques.h
+++ module-FO_S4/config_peripheriques.h
@@ -11,3 +11,8 @@ void PCA_Init(void);
 
 void Lumiere (void);
 void Lumiere_Stop (void);
+
+char* Envoi_Trame_SPI(void);
+int Reception_Trame_SPI(char* str);
+void SPI_Init(void);
+void it_SPI(void);
