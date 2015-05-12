d1626ea17144c4f64e6e3734d59cc0491b168084
LeoSchmitt <leo.schmitt@cpe.fr>
2015-05-12 17:55:26 +0200

    Update main.c

--- module-FO_S4/main.c
+++ module-FO_S4/main.c
@@ -16,6 +16,14 @@
 #endif	// Configuration globales du µprocesseur
 
 #include "config_peripheriques.h"
+
+
+#ifndef structure
+#define structure
+#include "structure.h"
+#endif
+
+#include <stdio.h>
 //------------------------------------------------------------------------------------
 // Global CONSTANTS
 //------------------------------------------------------------------------------------
@@ -26,62 +34,83 @@ unsigned char Lum_Nbre;
 unsigned int cpt;
 unsigned int nbOverflow;
 unsigned char ON;
+
+//sbit LED = P1^6;
+
+		
+/* pour la spi: ***********************************************/
+bit transmit_completed= 0;
+
+char tableau[20]; // stock chaine treçue SPI
+int index = 0; // index sur tableau
+
+int fin_spi=0;
+/******************** fonctions SPI *********************/
+
+// Reçoit des chaines de la forme "DD" + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + 0x00 + "FF"
+int Reception_Trame_SPI(char str[])
+{
+	
+	cmd_c.Etat_Lumiere = str[2];
+	cmd_c.Lumiere_Intensite = str[3];
+	cmd_c.Lumiere_Duree = str[4];
+	cmd_c.Lumiere_Extinction = str[5];
+	cmd_c.Lumiere_Nbre = str[6];
+	cmd_c.Etat_ACQ_Son = str[7];
+	cmd_c.ACQ_Duree = str[8];
+	return 1;
+}
+
+
 //------------------------------------------------------------------------------------
 // MAIN Routine
 //------------------------------------------------------------------------------------
 void main (void) {	
-	Intensite=80; // en %
+
+	/* initialisation des variables globales*/
+	
+	/* pour le laser : *************************************/
+	/*Intensite=80; // en %
 	Lum_ON=20;// choisir valeur entre 1 et 100
 	Lum_OFF=10;// choisir valeur entre 1 et 100
-	Lum_Nbre=10;
-	cpt=0;
-	ON=1;
-	nbOverflow=Lum_ON*4; // (x*10^-1)/(25*10^-3) rappel: un overflow tt les 25 ms
+	Lum_Nbre=10;*/
+	//char xdata machaine[50];
+	int xdata test;
+	//sprintf(machaine,"DD%c%c%c%c%c%c%cFF",0x01,0x10,0x01,0x10,0x04,0x00,0x00);
+	//test = Reception_Trame_SPI(machaine);
+	
+	/****************************************************/
 	Init_Device();
 	PCA_Init();
-	EA=1; // Autorisation interruptions
-	Lumiere();
+	SPI_Init();
+	
 	while (1) { // Boucle infinie
 		
-		
-		
-		
-		
-		switch(){
-			case :
-				
-				break;
+		if(fin_spi==1)
+		{
+			test=Reception_Trame_SPI(tableau);
+			index=0;
+			fin_spi=0;
 		}
+		
+		if(cmd_c.Etat_Lumiere==Allumer)
+		{
+			Intensite=cmd_c.Lumiere_Intensite;
+			Lum_ON=cmd_c.Lumiere_Duree;
+			Lum_OFF=cmd_c.Lumiere_Extinction;
+			Lum_Nbre=cmd_c.Lumiere_Nbre+1;
 			
+			cpt=0;
+			ON=1;
+			nbOverflow=Lum_ON*4; // (x*10^-1)/(25*10^-3) rappel: un overflow tt les 25 ms
+			Lumiere();
+			
+			cmd_c.Etat_Lumiere=Eteindre;
+		}
 		
-		
+		if(cmd_c.Etat_Lumiere==Eteindre)
+		{
+			Lumiere_Stop ();
+		}
 	}
 }
-
-typedef struct COMMANDES // Cette structure contient toutes les commandes envoyées par la centrale de commande
-// Les variables de type enum correspondent aux commandes
-// Les variables de type BYTE, UNSIGNED_BYTE ou int correspondent aux paramètres
-{
-enum Epreuve Etat_Epreuve; //Contient le numéro d'épreuve en cours - Commandes "D" "E" "Q"
-enum Mouvement Etat_Mouvement; //Contient le type de mouvement demandé - Commandes "A" "B" "S" "RD" "RG" "RC" "RA" "G"
-// Paramètres des commandes précédentes:
-BYTE Vitesse; // Paramètre vitesse - Peut aussi être modifié par la commande "TV"
-BYTE Coord_X; // Paramètre coordonnée X
-BYTE Coord_Y; // Paramètre coordonnée Y
-int Angle; // Paramètre angle
-enum ACQ_Son Etat_ACQ_Son; // Contient la commande d'acquisition de signaux sonores "ASS"
-// Paramètre de la commande précédente:
-BYTE ACQ_Duree; // Paramètre durée de la séquence d'acquisition
-enum DCT_Obst Etat_DCT_Obst; // Contient la commande de détection d'obstacle "MOB D" "MOB"
-// Paramètre de la commandes précédente:
-BYTE DCT_Obst_Resolution; // Paramètre résolution angulaire de la détection d'obstacle
-enum Lumiere Etat_Lumiere; // Contient la commande d'allumage du pointeur lumineux "L" "LS"
-// Paramètres de la commande précédente:
-BYTE Lumiere_Intensite; // Paramètre intensité lumineuse
-BYTE Lumiere_Duree; // Paramètre durée d'allumage
-BYTE Lumiere_Extinction; // Paramètre durée d'extinction
-BYTE Lumiere_Nbre; // Paramètre nombre de cycles d'allumage/extinction
-enum Servo Etat_Servo; // Contient la commande de positionnement d'un servo "CS C" "CS H" "CS V"
-// Paramètre de la commande précédente:
-BYTE Servo_Angle; //Paramètre angle de rotation
-} OUT_M1;
